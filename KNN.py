2000836fb44cd080784c81faac6a3ec9cc5d3de5
Gunish Katar <gunishkatar@gmail.com>
2018-07-21 23:07:25 +0530

    K-Nearest-Neighbour

---
+++ KNN.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def distance(v1, v2):
+	# Eucledian distance
+	return np.sqrt(((v1-v2)**2).sum())
+
+def knn(train, test, k=5):
+	dist = []
+	
+	for i in range(train.shape[0]):
+		# Get the vector and label
+		ix = train[i, :-1]
+		iy = train[i, -1]
+		# Compute the distance from test point
+		d = distance(test, ix)
+		dist.append([d, iy])
+	# Sort based on distance and get top k
+	dk = sorted(dist, key=lambda x: x[0])[:k]
+	# Retrieve only the labels
+	labels = np.array(dk)[:, -1]
+	
+	# Get frequencies of each label
+	output = np.unique(labels, return_counts=True)
+	# Find max frequency and corresponding label
+	index = np.argmax(output[1])
+	return output[0][index]
